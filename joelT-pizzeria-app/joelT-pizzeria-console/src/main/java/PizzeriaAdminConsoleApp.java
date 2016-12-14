a81cc8d182c03c72db56db0133dba724ddda3fe1
JoelT <joel.torib@gmail.com>
2016-12-14 06:55:51 +0100

    modif varaible pour application.properties

--- joelT-pizzeria-app/joelT-pizzeria-console/src/main/java/PizzeriaAdminConsoleApp.java
+++ joelT-pizzeria-app/joelT-pizzeria-console/src/main/java/PizzeriaAdminConsoleApp.java
@@ -1,6 +1,7 @@
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
-import fr.dao.specifique.PizzaDoaTableau;
+import fr.dao.PizzaDaoFactory;
 import fr.exception.ConsoleException;
 import fr.ihm.action.IhmUtil;
 import fr.ihm.action.MainMenu;
@@ -8,38 +9,30 @@ import fr.ihm.action.MainMenu;
 public class PizzeriaAdminConsoleApp {
 
 	public static void main(String[] args) throws ConsoleException {
-		/*
-		 * {ResourceBundle bundle = ResourceBundle.getBundle("application");}
-		 * Récuperation de la configuration dans le dossier Resource->
-		 * application.properties
-		 * 
-		 */
-		
-		
-		
-		// PizzaDaoFactory doaFactory;
-		// ResourceBundle bunble = ResourceBundle.getBundle(
-		// "application");/* recuperation dans le dossier application */
-		// String doaConfig = bunble.getString(
-		// "dao.impl"); /* recuperation de l'attribut stocker en String */
-		//
-		// try {
-		// doaFactory = (PizzaDaoFactory) Class.forName(doaConfig)
-		// .newInstance(); /*
-		// * creation de la classe nommée par
-		// * l'attribut stocker dans doaConfig
-		// */
-		//
-		// } catch (InstantiationException | IllegalAccessException |
-		// ClassNotFoundException e) {
-		// throw new ConsoleException(e);
-		//
-		// }
 
-		IhmUtil ihmUtil = new IhmUtil(new Scanner(System.in), new PizzaDoaTableau());
+		// Récuperation de la configuration dans le dossier Resource->
+		// application.properties
+
+		PizzaDaoFactory doaFactory;
+		ResourceBundle bunble = ResourceBundle.getBundle(
+				"application");/* recuperation dans le dossier application */
+		String doaConfig = bunble.getString(
+				"dao.impl"); /* recuperation de l'attribut stocker en String */
+
+		try {
+			doaFactory = (PizzaDaoFactory) Class.forName(doaConfig)
+					.newInstance(); /*
+									 * creation de la classe nommée par
+									 * l'attribut stocker dans doaConfig
+									 */
+
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			throw new ConsoleException(e);
+
+		}
+
+		IhmUtil ihmUtil = new IhmUtil(new Scanner(System.in), doaFactory);
 		MainMenu menu = new MainMenu(ihmUtil);
 		menu.start();
 	}
 }
-
-
